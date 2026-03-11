f4bc9af06e836a97c95e054bd05c1bb7e858ccfb
Realm.fr <lorenzodelaunay430@gmail.com>
2026-03-11 18:46:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-function scrollToContact(){
-document.getElementById("contact").scrollIntoView({
-behavior:"smooth"
-});
+```javascript
+const cards = document.querySelectorAll(".card")
+
+const observer = new IntersectionObserver(entries => {
+
+entries.forEach(entry => {
+
+if(entry.isIntersecting){
+
+entry.target.style.opacity = 1
+entry.target.style.transform = "translateY(0)"
+
 }
+
+})
+
+})
+
+cards.forEach(card=>{
+
+card.style.opacity = 0
+card.style.transform = "translateY(60px)"
+card.style.transition = "0.9s"
+
+observer.observe(card)
+
+})
+```
